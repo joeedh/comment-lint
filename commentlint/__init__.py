8d214d150d5dbb16ea62d33adcbf5dffea346bab
Joe Eagar <joeedh@gmail.com>
2026-08-30 07:47:32 -0700

    Release v0.3.6

--- commentlint/__init__.py
+++ commentlint/__init__.py
@@ -5,7 +5,7 @@ the whole point of the tool is to run from inside some other project.
 """
 import os
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RULES_PATH = os.path.join(ROOT, "data", "rules.json")
